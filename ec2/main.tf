35157577bbf1a82e92e98ca8f780bfa50e42ec56
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-05-12 22:41:22 +0900

    変数の利用

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,11 @@
+variable "example_instance_type" {
+    default = "t2.micro"
+}
+
 resource "aws_instance" "tf-example" {
     # TODO:最新のAmazon Linuxのバージョンを指定できるように変更する
     ami           = "ami-0bcc04d20228d0cf6"
-    instance_type = "t2.micro"
+    instance_type = var.example_instance_type
 
     tags = {
         Name = "tf-example"
