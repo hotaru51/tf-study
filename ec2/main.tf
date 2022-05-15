e038fe3d8814b21eb9559dff8d538d22b8759dbf
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-05-15 22:11:23 +0900

    ec2: データソースで最新のAmazon Linux 2のAMIを参照する

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,13 +1,27 @@
+data "aws_ami" "recent_amazon_linux_2" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+        name = "state"
+        values = ["available"]
+    }
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-5.10-hvm-?.?.????????.?-x86_64-gp2"]
+    }
+}
+
 variable "example_instance_type" {
     default = "t2.micro"
 }
 
 locals {
-    example_ami_id = "ami-0bcc04d20228d0cf6"
+    example_ami_id = data.aws_ami.recent_amazon_linux_2.image_id
 }
 
 resource "aws_instance" "tf-example" {
-    # TODO:最新のAmazon Linuxのバージョンを指定できるように変更する
     ami           = local.example_ami_id
     instance_type = var.example_instance_type
 
