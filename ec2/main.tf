7a322dfce4ac9b3bb434ac3379aef296c8b7c6d8
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-05-28 18:21:48 +0900

    セキュリティグループを追加

--- ec2/main.tf
+++ ec2/main.tf
@@ -25,9 +25,28 @@ locals {
     example_ami_id = data.aws_ami.recent_amazon_linux_2.image_id
 }
 
+resource "aws_security_group" "tf-example-sg" {
+    name = "tf-example-sg"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "tf-example" {
     ami           = local.example_ami_id
     instance_type = var.example_instance_type
+    vpc_security_group_ids = [aws_security_group.tf-example-sg.id]
 
     tags = {
         Name = "tf-example"
